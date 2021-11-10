d8886b1dbf9d8afdf34c8b622b0007d13e938a4f
Monkriz <sunseaseen@gmail.com>
2021-11-10 22:49:17 +0300

    Lesson 6

---
+++ src/Lesson6/EchoClient.java
@@ -0,0 +1,154 @@
+package Lesson6;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class EchoClient extends JFrame {
+
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 8189;
+
+    private JTextField msgInputField;
+    private JTextArea chatArea;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public EchoClient() {
+        try {
+            openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        prepareGUI();
+    }
+
+    public void openConnection() throws IOException {
+
+        socket = new Socket(SERVER_ADDR, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.equalsIgnoreCase("/end")) {
+                            out.writeUTF("Клиент отключился");
+                            closeConnection();
+                            break;
+                        }
+                        chatArea.append("Сервер: " + strFromServer);
+                        chatArea.append("\n");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    closeConnection();
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    public void sendMessage() {
+
+        if (!msgInputField.getText().trim().isEmpty()) {
+            try {
+                out.writeUTF(msgInputField.getText());
+                chatArea.append("Клиент: " + msgInputField.getText() + "\n");
+                msgInputField.setText("");
+                msgInputField.grabFocus();
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
+            }
+        }
+    }
+
+    public void prepareGUI() {
+
+        setBounds(600, 300, 500, 500);
+        setTitle("Клиент");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        add(new JScrollPane(chatArea), BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        JButton btnSendMsg = new JButton("Отправить");
+        bottomPanel.add(btnSendMsg, BorderLayout.EAST);
+        msgInputField = new JTextField();
+        add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.add(msgInputField, BorderLayout.CENTER);
+        btnSendMsg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+        msgInputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    out.writeUTF("/end");
+                    closeConnection();
+                } catch (IOException exc) {
+                    exc.printStackTrace();
+                } finally {
+                    closeConnection();
+                }
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new EchoClient();
+            }
+        });
+    }
+}
